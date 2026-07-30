40b2e45446a8827d07deee69372611bb4218c6b4
Salman Farsi <salmanfarsi9005@gmail.com>
2026-07-30 12:32:33 +0600

    feat: understand how C++ code runs

---
+++ fundamentals/cpp/variables-data-types-operators/how-does-code-run.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    // std::cout << "I want to be a best Programmer in this world" << endl;
+    cout << "I want to be a best Programmer in this world" << endl;
+    return 0;
+}
