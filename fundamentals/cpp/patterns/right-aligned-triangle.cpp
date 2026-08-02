d0308edf7a7ae90ad128606250d0cff72937e8f1
Salman Farsi <salmanfarsi9005@gmail.com>
2026-08-02 23:04:42 +0600

    feat: solve number triangle pattern problem

---
+++ fundamentals/cpp/patterns/right-aligned-triangle.cpp
@@ -0,0 +1,25 @@
+/*
+
+        *
+      * *
+    * * *
+  * * * *
+* * * * *
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+
+    for (int i = n; i >= 1; i--)
+    {
+
+        cout << "\n";
+    }
+
+    return 0;
+}
